629197913cf99490b7a3ecd1159d3b1bacdbc18f
Avinash9731550932 <avinash@connectia.in>
2023-09-06 11:01:52 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:weatherapp_starter_project/screens/home_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:weatherapp/cubits/weather/weather_cubit.dart';
+import 'package:weatherapp/services/weather_api_services.dart';
+import 'package:http/http.dart' as http;
+import 'cubits/temp_settings/temp_settings_cubit.dart';
+import 'pages/home_page.dart';
+import 'repositories/weather_repository.dart';
 
-void main() {
+void main() async {
+  await dotenv.load(fileName: '.env');
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: HomeScreen(),
-      title: "Weather",
-      debugShowCheckedModeBanner: false,
+    return RepositoryProvider(
+      create: (context) => WeatherRepository(
+          weatherApiServices: WeatherApiServices(httpClient: http.Client())),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<WeatherCubit>(
+            create: (context) => WeatherCubit(
+                weatherRepository: context.read<WeatherRepository>()),
+          ),
+          BlocProvider<TempSettingsCubit>(
+              create: (context) => TempSettingsCubit())
+        ],
+        child: MaterialApp(
+          title: 'Weather App',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const HomePage(),
+        ),
+      ),
     );
   }
 }
